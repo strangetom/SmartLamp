e8f25c3cde5d0b12f955afdbfa3ebe0c5c1765fe
Tom Strange <tpstrange@gmail.com>
2016-11-20 21:09:25 +0000

    Change method of choosing static colour to colour picker

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -12,40 +12,16 @@ uint16_t j = 0; // determine colour of LEDs
 Adafruit_NeoPixel ring = Adafruit_NeoPixel(16, PIN, NEO_GRB + NEO_KHZ800);
 // Colours for static mode
 uint8_t red, green, blue;
+String hexColour = "#3d34b9";
 
 const char* ssid = "ssid";
 const char* pass = "pass";  
 int status = WL_IDLE_STATUS;
 ESP8266WebServer server(80);
 
-String webPage = "";
 char* lampMode = "rainbow";
 
 void setup() {
-  // Set webpage to display
-  webPage += "<!DOCTYPE html>\
-  <html>\
-    <head>\
-      <title>ESP8266 Server</title>\
-      <meta name='viewport' content='width=device-width, initial-scale=1'>\
-      <style type='text/css'>html{background-color:#fff;}body{margin:0 auto;max-width:50em;line-height:1.5;padding:2em 1em;border:40px solid #fff;border-radius:45px;background:#F0F0F0;color:#566b78;font-family:sans-serif;-moz-font-smoothing:grayscale;-webkit-font-smoothing:antialiased}h1,strong{color:#333;font-weight:500}a{color:#e81c4f}</style>\
-    </head>\
-    <body>\
-    <center><h1>ESP8266 LED LAMP CONTROL</h1></center>\
-    <p> Put the lamp in <a href=\"rainbow\">rainbow</a> mode, or</p>\
-    <p> put the lamp in <a href=\"warm\">warm</a> mode, or</p>\
-    <p> put the lamp in <a href=\"cold\">cold</a> mode, or maybe try</p>\
-    <p> <form id='static' action='rgb' method='post' oninput=\"rgb.value = Red.value+' '+Green.value+' '+Blue.value\">\
-      <a onclick=\"document.getElementById('static').submit();\" style='cursor: pointer;text-decoration: underline;'>static</a> mode:\
-      R <input type='text' name='R' size='1' value='255'>\ 
-      G <input type='text' name='G' size='1' value='255'>\ 
-      B <input type='text' name='B' size='1' value='255'>\ 
-      <output name='rgb'> </output>\
-    </form> </p>\
-    <p> Finally, we can turn the lamp <a href=\"off\">off.</a> </p>\
-    </body>\
-  </html>";
-
   // Initialise serial for debugging
   Serial.begin(115200);
   Serial.println("Attempting to connect to network...");
@@ -63,34 +39,38 @@ void setup() {
   
   // Functions for lamp settings
   server.on("/", [](){
-      server.send(200, "text/html", webPage);
+      server.send(200, "text/html", buildWebpage(hexColour));
     });
 
   server.on("/rainbow", [](){
-    server.send(200, "text/html", webPage);
+    server.send(200, "text/html", buildWebpage(hexColour));
     lampMode = "rainbow";
   });
 
   server.on("/warm", [](){
-    server.send(200, "text/html", webPage);
+    server.send(200, "text/html", buildWebpage(hexColour));
     lampMode = "warm";
   });
 
   server.on("/cold", [](){
-    server.send(200, "text/html", webPage);
+    server.send(200, "text/html", buildWebpage(hexColour));
     lampMode = "cold";
   });
 
   server.on("/off", [](){
-    server.send(200, "text/html", webPage);
+    server.send(200, "text/html", buildWebpage(hexColour));
     lampMode = "off";
   });
 
-  server.on("/rgb", [](){
-    server.send(200, "text/html", webPage);;
-    red = server.arg(0).toInt();
-    green = server.arg(1).toInt();
-    blue = server.arg(2).toInt();
+  server.on("/colour", [](){
+    hexColour = server.arg(0);
+    server.send(200, "text/html", buildWebpage(hexColour));;
+
+    // Convert the hex number to rgb values
+    int number = (int)strtol( &hexColour[1], NULL, 16);
+    red = number >> 16;
+    green = number >> 8 & 0xFF;
+    blue = number & 0xFF;
     lampMode = "static";
   });
 
@@ -123,7 +103,12 @@ void loop() {
     for(uint16_t i=0; i<ring.numPixels(); i++) {
       ring.setPixelColor(i, ring.Color(red, green, blue));
     }
-    ring.show();       
+    ring.show(); 
+    if(j == 255){
+      j = 0;
+    }else{
+      j++;
+    }      
     Serial.println("LED static: "  + String(j));
     
   } else if (strcmp(lampMode, "warm") == 0){
@@ -163,6 +148,33 @@ void loop() {
 
   delay(1000);
   
+}
+
+// Build webpage with correct colour for colour picker
+String buildWebpage(String hexColour){
+  String webPage = "";
+  webPage += "<!DOCTYPE html>\
+  <html>\
+    <head>\
+      <title>ESP8266 Server</title>\
+      <meta name='viewport' content='width=device-width, initial-scale=1'>\
+      <style type='text/css'>html{background-color:#fff;}body{margin:0 auto;max-width:50em;line-height:1.5;padding:2em 1em;border:40px solid #fff;border-radius:45px;background:#F0F0F0;color:#566b78;font-family:sans-serif;-moz-font-smoothing:grayscale;-webkit-font-smoothing:antialiased}h1,strong{color:#333;font-weight:500}a{color:#e81c4f}</style>\
+    </head>\
+    <body>\
+    <center><h1>ESP8266 LED LAMP CONTROL</h1></center>\
+    <p> Put the lamp in <a href=\"rainbow\">rainbow</a> mode, or</p>\
+    <p> put the lamp in <a href=\"warm\">warm</a> mode, or</p>\
+    <p> put the lamp in <a href=\"cold\">cold</a> mode, or maybe try</p>\
+    <p> <form id='static' action='colour' method='post'>\
+      static mode:\
+      <input type='color' name='colour' value='";
+   webPage += hexColour;
+   webPage += "' style='width:30%' onchange=\"document.getElementById('static').submit();\">\
+    </form> </p>\
+    <p> Finally, we can turn the lamp <a href=\"off\">off.</a> </p>\
+    </body>\
+  </html>";
+  return webPage;
 }
 
 // Turn LEDs off
